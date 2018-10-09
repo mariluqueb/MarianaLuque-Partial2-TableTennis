2f65a424787c6e5fcccdc711294b799ef8430f00
mariluqueb <mabelu_96@hotmail.com>
2018-10-08 22:37:01 -0500

    Parcial segundo corte

---
+++ UnityScriptingBasics/Assets/Scripts/RightRacket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightRacket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * 0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * 0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * 0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * 0.5f);
+        }
+	}
+}
